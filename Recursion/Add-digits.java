564661ba12512fe555a8d0d5403c1cc4aafe1797
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-04-29 10:38:03 +0530

    Create Add-digits.java

---
+++ Recursion/Add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num < 10) return num;
+        int sum = 0;
+        while(num != 0){
+            sum += num % 10;
+            num = num / 10;
+        }
+        // System.out.println(sum);
+
+        return addDigits(sum);
+    }
+}
