14b4c1b5d1847c7104d6242136920c18ea2629bf
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-05-01 17:01:54 +0530

    Update Merge-Sorted-Array.java

--- Two Pointers/Merge-Sorted-Array.java
+++ Two Pointers/Merge-Sorted-Array.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int index = m + n - 1;
+        while(ptr2 >= 0){
+            if(ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]){
+                nums1[index] = nums1[ptr1];
+                ptr1--;
+                index--;
+            }else{
+                nums1[index] = nums2[ptr2];
+                ptr2--;
+                index--;
+            }
+        }
+    }
+}
