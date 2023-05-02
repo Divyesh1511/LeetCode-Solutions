0f804702198923b05d5e8d298726f9b760777978
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-05-02 15:51:15 +0530

    Update average-salary-excluding-the-minimum-and-maximum-salary.java

--- Sorting/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ Sorting/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        double avg = 0;
+        for(int i = 1; i < salary.length - 1; i++){
+            avg += salary[i];
+        }
+        return (avg/(salary.length - 2));
+    }
+}
