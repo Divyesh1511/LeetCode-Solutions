0caafb466eb83c4a030a374ce7384b186e923e5a
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-05-08 18:13:51 +0530

    Update matrix-diagonal-sum.java

--- Matrix (2D Array)/matrix-diagonal-sum.java
+++ Matrix (2D Array)/matrix-diagonal-sum.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int i = 0, j = 0, n = mat.length, m = mat[0].length;
+        int sum = 0;
+        while(i < n && j < m){
+            sum += mat[i][j] + mat[i][m-1-j];
+            i++;
+            j++;
+        }
+        if(n % 2 != 0){
+            sum -= mat[n/2][m/2];
+        }
 
+        return sum;
+    }
+}
