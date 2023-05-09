2745a51104ce534f0433c28ecfd96f2ef4b94746
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-05-09 15:37:52 +0530

    Update spiral-matrix.java

--- Matrix (2D Array)/spiral-matrix.java
+++ Matrix (2D Array)/spiral-matrix.java
@@ -1,1 +1,35 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
 
+        int left = 0, right = matrix[0].length-1, top = 0, bottom = matrix.length-1;
+
+        while(left <= right && top <= bottom){
+            for(int i = left; i <= right; i++){
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i = top; i <= bottom; i++){
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if(top <= bottom){
+                for(int i = right; i >= left; i--){
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left <= right){
+                for(int i = bottom; i >= top; i--){
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
