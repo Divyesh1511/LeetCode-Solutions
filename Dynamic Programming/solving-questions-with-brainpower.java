4920b89b8477c9bde59606489a818276b2f3e231
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-05-12 23:34:06 +0530

    Update solving-questions-with-brainpower.java

--- Dynamic Programming/solving-questions-with-brainpower.java
+++ Dynamic Programming/solving-questions-with-brainpower.java
@@ -1,1 +1,20 @@
+class Solution {
+    public long mostPoints(int[][] questions) {
+        long[] dp = new long[questions.length];
 
+        for(int i = questions.length-1; i >= 0; i--){
+            int FI = i + questions[i][1] + 1;  // FI (Future Index)
+            if(FI < questions.length){
+                dp[i]  = dp[FI] + questions[i][0];
+            }else{
+                dp[i] = questions[i][0];
+            }
+
+            if(i < questions.length-1){
+                dp[i] = Math.max(dp[i+1], dp[i]);
+            }
+        }
+
+        return dp[0];
+    }
+}
