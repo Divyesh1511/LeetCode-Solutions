0bd2d74f44edba5b03d73dfb5f8554d121dced65
Divyesh Khuva <75933218+Divyesh1511@users.noreply.github.com>
2023-10-07 09:41:07 +0530

    Update 343-Integer-Break.java

--- Math/343-Integer-Break.java
+++ Math/343-Integer-Break.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n <= 3) return n-1;
+        int q = n / 3;
+        int r = n % 3;
+        if(r == 1){
+            return ((int)Math.pow(3, q-1) * 4);
+        }
+        if(r == 2){
+            return ((int)Math.pow(3, q) * 2);
+        }
 
+        return (int)Math.pow(3, q);
+    }
+}
